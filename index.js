ecd933fd57a46c4b4e77dd51ceb42692db0951d9
ShivamChanana <shivchanana77@gmail.com>
2019-11-28 20:32:45 +0530

    deploying crud9

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 5000
+const port = process.env.PORT || 5000
 const mongoose = require('mongoose')
 const methodOverride = require ('method-override')
 
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended : false}))
 app.use(methodOverride('_method'))
 
-mongoose.connect('mongodb://localhost:27017/users', (err)=>{
+mongoose.connect('mongodb+srv://Shivam:shivam@1811@cluster0-2aufw.mongodb.net/test?retryWrites=true&w=majority', (err)=>{
     if(err){
         console.log('err')
     }else{
